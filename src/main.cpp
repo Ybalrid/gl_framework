2f2ed6dde0e0c6fd42774f40a4ed2d3a31a914d9
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2019-02-23 19:05:10 +0100

    updated cppsdl2

--- src/main.cpp
+++ src/main.cpp
@@ -2,42 +2,69 @@
 #define CPP_SDL2_GL_WINDOW
 #include <cpp-sdl2/sdl.hpp>
 
+#include <iostream>
+
 int main(int argc, char* argv[])
 {
-    (void)(argc);
-    (void)(argv);
+	(void)(argc);
+	(void)(argv);
 
-    //init sdl
+	//init sdl
 	auto root = sdl::Root(SDL_INIT_EVERYTHING);
-    sdl::Event event;
+	sdl::Event event;
 
-    //create window
-    auto window = sdl::Window("application window",
-        {1024,768}, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+	//create window
+	auto window = sdl::Window("application window",
+			{1024,768}, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
 
-    //create opengl context
-    auto context = window.create_context();
-    context.make_current();
+	//create opengl context
+	auto context = window.create_context();
+	context.make_current();
+	
+	if(glewInit() != GLEW_OK)
+	{
+		std::cerr << "cannot init glew\n";
+		abort();
+	}
 
-    /*//set vsync mode
-    try
-    {
-        window.gl_set_swap_interval(sdl::Window::gl_swap_interval::adaptive_vsync);
-    }
-    catch (const std::exception& e)
-    {
-        (void)(e);
-        window.gl_set_swap_interval(sdl::Window::gl_swap_interval::vsync);
-    }*/
 
-    //set opengl clear color
-    glClearColor(0.2f, 0.3f, 0.4f, 1);
+	std::cout << "OpenGL " << glGetString(GL_VERSION) << '\n';
+	//set vsync mode
+	try
+	{
+		window.gl_set_swap_interval(sdl::Window::gl_swap_interval::adaptive_vsync);
+	}
+	catch (sdl::Exception e)
+	{
+		std::cerr << e.what() << '\n';
+		std::cerr << "Using standard vsync instead of adaptive vsync\n";
+		try
+		{
+			window.gl_set_swap_interval(sdl::Window::gl_swap_interval::vsync);
+		}
+		catch(sdl::Exception e)
+		{
+			std::cerr << e.what() << '\n';
+			std::cerr << "Cannot set vsync for this driver.\n";
+			try
+			{
+				window.gl_set_swap_interval(sdl::Window::gl_swap_interval::immediate);
+			}
+			catch(sdl::Exception e)
+			{
+				std::cerr << e.what() << '\n';
+			}
+		}
+	}
 
-    //main loop
-    auto run = true;
+	//set opengl clear color
+	glClearColor(0.2f, 0.3f, 0.4f, 1);
+
+	//main loop
+	auto run = true;
 	while(run)
 	{
-        //event polling
+		//event polling
 		while(event.poll())
 		{
 			switch(event.type)
@@ -47,12 +74,12 @@ int main(int argc, char* argv[])
 			}
 		}
 
-        //clear viewport
-        glClear(GL_COLOR_BUFFER_BIT);
+		//clear viewport
+		glClear(GL_COLOR_BUFFER_BIT);
 
 
-        //swap buffers
-        window.gl_swap();
+		//swap buffers
+		window.gl_swap();
 	}
 
 	return 0;
