89cfb8851cc1ad3d505d2a547e374047f4eaf240
Ybalrid (Arthur Brainville) <ybalrid@ybalrid.info>
2021-09-06 02:56:47 +0200

    moved physics_system to its own compilation unit

---
+++ src/physics_system.cpp
@@ -0,0 +1,147 @@
+#include "physics_system.hpp"
+
+physics_system::physics_system()
+{
+  std::cout << "Initializing physics system with Bullet Physics version " << btGetVersion() << " : " << btIsDoublePrecision()
+            << "\n";
+  collision_configuration = std::make_unique<btDefaultCollisionConfiguration>();
+  collision_dispatcher    = std::make_unique<btCollisionDispatcher>(collision_configuration.get());
+  overlapping_pair_cache  = std::make_unique<btDbvtBroadphase>();
+  dynamics_world          = std::make_unique<btDiscreteDynamicsWorld>(
+      collision_dispatcher.get(), overlapping_pair_cache.get(), solver.get(), collision_configuration.get());
+
+  dynamics_world->setDebugDrawer(&simple_debug_drawer);
+}
+
+physics_system::~physics_system() { std::cout << "Deinitializing physics system\n"; }
+
+void physics_system::set_gravity(glm::vec3 G) const { dynamics_world->setGravity(bullet_utils::convert(G)); }
+
+void physics_system::step_simulation(float delta_time_seconds) const
+{
+  dynamics_world->stepSimulation(
+      delta_time_seconds, simulation_configuration.substep_per_frame, 1.F / simulation_configuration.substep_simulation_fps);
+}
+
+void physics_system::physics_proxy::compute_inertia()
+{
+  if(!collision_shape) return;
+  local_inertia = btVector3(0, 0, 0);
+  if(mass == 0) return;
+  collision_shape->calculateLocalInertia(mass, local_inertia);
+}
+
+void physics_system::physics_proxy::create_default_motion_state() { create_motion_state<btDefaultMotionState>(); }
+
+void physics_system::physics_proxy::create_rigid_body()
+{
+  btRigidBody::btRigidBodyConstructionInfo rb_info(mass, motion_state.get(), collision_shape.get(), local_inertia);
+  rigid_body = std::make_unique<btRigidBody>(rb_info);
+}
+
+transform physics_system::physics_proxy::get_world_transform() const
+{
+  if(!rigid_body) return transform {};
+
+  btTransform world_xform {};
+  rigid_body->getMotionState()->getWorldTransform(world_xform);
+
+  return bullet_utils::convert(world_xform);
+}
+
+physics_system::box_proxy::box_proxy(glm::vec3 start_position, glm::vec3 half_extent, float mass_) : physics_proxy()
+{
+  const btVector3 box_size = bullet_utils::convert(half_extent);
+  xform.setOrigin(bullet_utils::convert(start_position));
+
+  collision_shape = std::make_unique<btBoxShape>(box_size);
+
+  mass = mass_;
+  compute_inertia();
+  create_default_motion_state();
+  create_rigid_body();
+}
+
+physics_system::box_proxy::box_proxy(float size, float mass_)
+{
+
+  const auto half_size = size / 2.f;
+  glm::vec3 half_extent { half_size, half_size, half_size };
+  *this = box_proxy({}, half_extent, mass);
+}
+
+void physics_system::add_to_world(physics_proxy& proxy) const { dynamics_world->addRigidBody(proxy.rigid_body.get()); }
+
+void physics_system::add_ground_plane()
+{
+  fake_ground_plane = std::make_unique<box_proxy>(glm::vec3(0, -1, 0), glm::vec3(500, 1, 500), 0);
+  add_to_world(*fake_ground_plane);
+}
+
+void physics_system::draw_phy_debug(const glm::mat4& view, const glm::mat4& projection, GLuint vao, shader_handle shader)
+{
+  simple_debug_drawer.new_frame();
+  dynamics_world->debugDrawWorld();
+  simple_debug_drawer.draw_debug_data(view, projection, vao, shader);
+}
+
+void physics_system::debug_drawer::drawLine(const btVector3& from, const btVector3& to, const btVector3& color)
+{
+  to_draw.push_back({ bullet_utils::convert(from), bullet_utils::convert(to), bullet_utils::convert(color) });
+}
+
+void physics_system::debug_drawer::new_frame() { to_draw.clear(); }
+
+void physics_system::debug_drawer::draw_debug_data(const glm::mat4& view,
+                                                   const glm::mat4& projection,
+                                                   GLuint vao,
+                                                   shader_handle shader_h)
+{
+  glm::vec3 line_vertex_data[2] {}; //here's a bit of stack to give a pointer to OpenGL for where to find 6 floats
+
+  opengl_debug_group debug_group("physics_system::debug_draw::draw_debug_data()");
+
+  //Note, this is a debug feature, and exist only outside of the renderer. This will corrupt the VAO state tracking, so manually restore the currently bound VAO
+  GLuint vao_to_be_restored;
+  glGetIntegerv(GL_VERTEX_ARRAY_BINDING, (GLint*)&vao_to_be_restored);
+
+  glBindVertexArray(vao);
+
+  auto& shader = shader_program_manager::get_from_handle(shader_h);
+  shader.use();
+  shader.set_uniform(shader::uniform::view, view);
+  shader.set_uniform(shader::uniform::projection, projection);
+
+
+  for(auto& line : to_draw)
+  {
+    glm::vec4 color = { line.color, 1 };
+    shader.set_uniform(shader::uniform::debug_color, color);
+    line_vertex_data[0] = line.from;
+    line_vertex_data[1] = line.to;
+    glBufferData(GL_ARRAY_BUFFER, 2 * 3 * sizeof(float), line_vertex_data, GL_STREAM_DRAW);
+    glDrawElements(GL_LINES, 2, GL_UNSIGNED_SHORT, (void*)0);
+  }
+
+  glBindVertexArray(vao_to_be_restored);
+}
+
+void physics_system::debug_drawer::drawContactPoint(
+    const btVector3& PointOnB, const btVector3& normalOnB, btScalar distance, int lifeTime, const btVector3& color)
+{
+  drawLine(PointOnB, PointOnB + normalOnB * distance * 20, color);
+}
+
+void physics_system::debug_drawer::reportErrorWarning(const char* warningString)
+{
+  std::cerr << "physics error : " << warningString << "\n";
+}
+
+void physics_system::debug_drawer::draw3dText(const btVector3& location, const char* textString)
+{
+  //Note if we ever add something that can draw 3D text, well... We will use it here.
+}
+
+void physics_system::debug_drawer::setDebugMode(int debugMode) { debug_mode = debugMode; }
+
+int physics_system::debug_drawer::getDebugMode() const { return debug_mode; }
--- src/physics_system.hpp
+++ src/physics_system.hpp
@@ -2,6 +2,11 @@
 
 #include <memory>
 
+#include <vector>
+#include <iostream>
+#include "shader_program_manager.hpp"
+#include "opengl_debug_group.hpp"
+
 #include "btBulletDynamicsCommon.h"
 #include "btBulletCollisionCommon.h"
 
@@ -42,32 +47,13 @@ public:
     float substep_simulation_fps = 250;
   } simulation_configuration;
 
-  physics_system()
-  {
-    std::cout << "Initializing physics system with Bullet Physics version " << btGetVersion() << " : " << btIsDoublePrecision() << "\n";
-    collision_configuration = std::make_unique<btDefaultCollisionConfiguration>();
-    collision_dispatcher    = std::make_unique<btCollisionDispatcher>(collision_configuration.get());
-    overlapping_pair_cache  = std::make_unique<btDbvtBroadphase>();
-    dynamics_world          = std::make_unique<btDiscreteDynamicsWorld>(
-        collision_dispatcher.get(), overlapping_pair_cache.get(), solver.get(), collision_configuration.get());
+  physics_system();
 
+  ~physics_system();
 
-    dynamics_world->setDebugDrawer(&my_drawer);
-  }
+  void set_gravity(glm::vec3 G) const;
 
-  ~physics_system() { std::cout << "Deinitializing physics system\n";
-  }
-
-  void set_gravity(glm::vec3 G)
-  {
-    dynamics_world->setGravity(bullet_utils::convert(G));
-  }
-
-  void step_simulation(float delta_time_seconds)
-  {
-    dynamics_world->stepSimulation(
-        delta_time_seconds, simulation_configuration.substep_per_frame, 1.F / simulation_configuration.substep_simulation_fps);
-  }
+  void step_simulation(float delta_time_seconds) const;
 
   struct physics_proxy
   {
@@ -75,13 +61,7 @@ public:
     btVector3 local_inertia;
     btTransform xform = btTransform::getIdentity();
 
-    void compute_inertia()
-    {
-      if(!collision_shape) return;
-      local_inertia = btVector3(0, 0, 0);
-      if(mass == 0) return;
-      collision_shape->calculateLocalInertia(mass, local_inertia);
-    }
+    void compute_inertia();
 
     template<class MotionState>
     void create_motion_state()
@@ -89,27 +69,11 @@ public:
       motion_state = std::make_unique<MotionState>(xform);
     }
 
-    void create_default_motion_state()
-    {
-      create_motion_state<btDefaultMotionState>();
-    }
+    void create_default_motion_state();
 
-    void create_rigid_body()
-    {
-      btRigidBody::btRigidBodyConstructionInfo rb_info(mass, motion_state.get(), collision_shape.get(), local_inertia);
-      rigid_body               = std::make_unique<btRigidBody>(rb_info);
-    }
+    void create_rigid_body();
 
-    transform get_world_transform() const
-    {
-      if(!rigid_body) return transform {};
-
-
-      btTransform world_xform {};
-      rigid_body->getMotionState()->getWorldTransform(world_xform);
-
-      return bullet_utils::convert(world_xform);
-    }
+    transform get_world_transform() const;
 
     std::unique_ptr<btCollisionShape> collision_shape{nullptr};
     std::unique_ptr<btMotionState> motion_state { nullptr };
@@ -118,43 +82,16 @@ public:
 
   struct box_proxy : physics_proxy
   {
-    box_proxy(glm::vec3 start_position, glm::vec3 half_extent, float mass_) : physics_proxy()
-    {
-      const btVector3 box_size = bullet_utils::convert(half_extent);
-      xform.setOrigin(bullet_utils::convert(start_position));
+    box_proxy(glm::vec3 start_position, glm::vec3 half_extent, float mass_);
 
-      collision_shape          = std::make_unique<btBoxShape>(box_size);
-
-      mass = mass_;
-      compute_inertia();
-      create_default_motion_state();
-      create_rigid_body();
-    }
-
-    box_proxy(float size, float mass_) {
-
-      const auto half_size = size / 2.f;
-      glm::vec3 half_extent { half_size, half_size, half_size };
-      *this = box_proxy({} , half_extent, mass);
-    }
+    box_proxy(float size, float mass_);
   };
 
-  void add_to_world(physics_proxy& proxy)
-  { dynamics_world->addRigidBody(proxy.rigid_body.get());
-  }
+  void add_to_world(physics_proxy& proxy) const;
 
-  void add_ground_plane()
-  {
-    fake_ground_plane = std::make_unique<box_proxy>(glm::vec3(0, -1, 0), glm::vec3(500, 1, 500), 0);
-    add_to_world(*fake_ground_plane);
-  }
+  void add_ground_plane();
 
-  void draw_phy_debug(const glm::mat4& view, const glm::mat4& projection, GLuint vao, shader_handle shader)
-  {
-    my_drawer.new_frame();
-    dynamics_world->debugDrawWorld();
-    my_drawer.draw_debug_data(view, projection, vao, shader);
-  }
+  void draw_phy_debug(const glm::mat4& view, const glm::mat4& projection, GLuint vao, shader_handle shader);
 
 private:
 
@@ -178,62 +115,25 @@ private:
 
     int debug_mode = DBG_DrawWireframe | DBG_DrawAabb | DBG_DrawContactPoints;
 
-    void drawLine(const btVector3& from, const btVector3& to, const btVector3& color) override {
-      to_draw.push_back({ bullet_utils::convert(from), bullet_utils::convert(to), bullet_utils::convert(color) });
-    }
+    void drawLine(const btVector3& from, const btVector3& to, const btVector3& color) override;
 
-    void new_frame()
-    {
-      to_draw.clear();
-    }
+    void new_frame();
 
-    void draw_debug_data(const glm::mat4& view, const glm::mat4& projection, GLuint vao, shader_handle shader_h)
-    {
-      GLuint restore_vao;
-      glGetIntegerv(GL_VERTEX_ARRAY_BINDING, (GLint*)&restore_vao);
-
-      glBindVertexArray(vao);
-
-      auto& shader = shader_program_manager::get_from_handle(shader_h);
-      shader.use();
-      shader.set_uniform(shader::uniform::view, view);
-      shader.set_uniform(shader::uniform::projection, projection);
-
-      glm::vec3 line_vertex_data[2];
-      for(auto& line : to_draw)
-      {
-        glm::vec4 color = { line.color, 1 };
-        shader.set_uniform(shader::uniform::debug_color, color);
-        line_vertex_data[0] = line.from;
-        line_vertex_data[1] = line.to;
-        glBufferData(GL_ARRAY_BUFFER, 2 * 3 * sizeof(float), line_vertex_data, GL_STREAM_DRAW);
-        glDrawElements(GL_LINES, 2, GL_UNSIGNED_SHORT, (void*)0);
-      }
-      glBindVertexArray(restore_vao);
-    }
+    void draw_debug_data(const glm::mat4& view, const glm::mat4& projection, GLuint vao, shader_handle shader_h);
 
     void drawContactPoint(const btVector3& PointOnB,
                           const btVector3& normalOnB,
                           btScalar distance,
                           int lifeTime,
-                          const btVector3& color) override
-    {
-      drawLine(PointOnB, PointOnB + normalOnB * distance * 20, color);
-    }
+                          const btVector3& color) override;
 
-    void reportErrorWarning(const char* warningString) override { std::cerr << "physics error : " << warningString << "\n";}
+    void reportErrorWarning(const char* warningString) override;
 
-    void draw3dText(const btVector3& location, const char* textString) override { }
+    void draw3dText(const btVector3& location, const char* textString) override;
 
-    void setDebugMode(int debugMode) override
-    {
-      debug_mode = debugMode;
-    }
+    void setDebugMode(int debugMode) override;
 
-    int getDebugMode() const override
-    {
-      return debug_mode;
-    }
+    int getDebugMode() const override;
   };
 
   debug_drawer my_drawer;
